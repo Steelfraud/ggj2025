6ebb7de35eee6080a86d6f9df5f399bfedd023cf
Sakke <ekq.sakke@gmail.com>
2025-01-24 23:07:10 +0200

    Target group functions

--- Assets/Scripts/Camera/CustomCamera.cs
+++ Assets/Scripts/Camera/CustomCamera.cs
@@ -1,7 +1,25 @@
+using Unity.Cinemachine;
 using UnityEngine;
-using Cinemachine;
 
 public class CustomCamera : MonoBehaviour
 {
     public CinemachineTargetGroup targetGroup;
+
+    public void AddToTargetGroup(Transform transform)
+    {
+        if (targetGroup == null || targetGroup.FindMember(transform) != -1)
+        {
+            return;
+        }
+        targetGroup.AddMember(transform, 1, .5f);
+    }
+
+    public void RemoveFromTargetGroup(Transform transform)
+    {
+        if (targetGroup == null || targetGroup.FindMember(transform) != -1)
+        {
+            return;
+        }
+        targetGroup.RemoveMember(transform);
+    }
 }
