33ad8049df6e29568ea3d8edfc29a30834099d18
Mikko Österman <mikko.osterman@dc.lab>
2025-01-25 16:09:47 +0200

    Rocket fine tuning

--- Assets/Scripts/Game/PickUpS/RocketPickUp.cs
+++ Assets/Scripts/Game/PickUpS/RocketPickUp.cs
@@ -6,9 +6,14 @@ public class RocketPickUp : PlayerPickUpObjectBase
     [SerializeField]
     private Rocket rocketPrefab;
 
-    protected override void ApplyEffect(Player player)
+    protected override void ApplyEffect(PlayerAvatar player)
     {
         var direction = player.transform.forward;
+
+        if (GameManager.Instance.activePlayers.Count > 1)
+        {
+            direction = GameManager.Instance.activePlayers.GetRandomElementFromList().transform.position;
+        }
 
         var rocket = Instantiate(rocketPrefab, player.transform.position + 2 * direction, Quaternion.FromToRotation(transform.position, direction));
         base.ApplyEffect(player);
