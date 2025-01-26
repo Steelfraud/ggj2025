241032b367496f6d552048b01bafdb6f6ced2be4
Mikko Österman <mikko.osterman@dc.lab>
2025-01-26 11:46:46 +0200

    Added particles to BlastPickUp

--- Assets/Scripts/Game/PickUpS/DestroyBubblesPickUp.cs
+++ Assets/Scripts/Game/PickUpS/DestroyBubblesPickUp.cs
@@ -8,9 +8,13 @@ public class DestroyBubblesPickUp : PlayerPickUpObjectBase
 {
     [SerializeField]
     private float blastRadius = 5f;
+    [SerializeField]
+    private GameObject particles;
 
     protected override void ApplyEffect(PlayerAvatar player)
     {
+        Instantiate(particles, transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity);
+
         Vector3 p1 = transform.position;
         RaycastHit[] hits = Physics.SphereCastAll(p1, blastRadius, transform.forward, 10f);
 
